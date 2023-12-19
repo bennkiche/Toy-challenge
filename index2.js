929984eb1b10c3894a61b571118188de1a2442e4
Benn <bennkiche7@gmail.com>
2023-12-18 19:41:57 -0800

    second challenge

---
+++ index2.js
@@ -0,0 +1,15 @@
+function detector (speed) {
+    if (speed <0 || speed > 130){
+        return "abnormal"
+    }
+    else if  (speed <= 70) {
+        return "ok"
+    }
+    else if (speed >70 <=80) {
+        return "points 2"
+    }
+    else if (speed >=130) {
+        return "license suspended"
+    }
+}
+console.log (speed(65))
