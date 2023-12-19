29a0a6c6dc36369e7f579db8acda78c48fd83e10
Benn <bennkiche7@gmail.com>
2023-12-18 18:34:33 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function grading (score){
+    if (score < 0 || score > 100) {
+        return "invalid mark"
+    }
+    else if (score <= 40){
+        return "E"
+    }
+     else if  (score <= 49) {
+        return "D"
+    }
+    else if  (score <= 59) {
+        return "C"
+    }
+    else if (score <= 79) {
+        return "B"
+    }
+    else if (score <= 100){
+        return "A"
+    }
+}
+console.log (grading (50))
